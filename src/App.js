2b5fe96af31f32398fb2a532366fbe18bebefe94
gurdalc19 <79787722+gurdalc19@users.noreply.github.com>
2023-05-26 03:48:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Cihangir Hoca Bitirme
+          Cihangir Özemir Hoca Bitirme
         </a>
       </header>
     </div>
